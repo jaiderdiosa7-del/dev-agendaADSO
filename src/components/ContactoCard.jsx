47bf5a22c13c6e4696bb51e67798f31fa22dc054
Jaider Diosa <jaiderdiosa7@gmail.com>
2026-08-11 16:56:53 -0500

    Clase_6_Tailwind_UI_Agenda_ADSO_v3

--- src/components/ContactoCard.jsx
+++ src/components/ContactoCard.jsx
@@ -1,15 +1,38 @@
-export default function ContactoCard({ nombre, telefono, correo, etiqueta, onEliminar }) {
-  return (
-    <article className="tarjeta-contacto">
-      <h3>{nombre}</h3>
-      <p>📞 {telefono}</p>
-      <p>✉️ {correo}</p>
-      {etiqueta && <p>{etiqueta}</p>}
-      <div className="acciones">
-        <button className="btn-eliminar" onClick={() => onEliminar(correo)}>
-          Eliminar
-        </button>
-      </div>
-    </article>
-  );
+// src/components/ContactoCard.jsx
+export default function ContactoCard({
+ nombre,
+ telefono,
+ correo,
+ empresa,
+ etiqueta,
+ onEliminar
+}) {
+ return (
+ <article className="bg-white border border-gray-200 rounded-xl shadow-sm p-5">
+ {/* Nombre destacado */}
+ <h3 className="text-xl font-bold text-white-900 mb-3">
+ {nombre}
+ </h3>
+ {/* Teléfono */}
+ <p className="text-gray-700 mb-1"></p>
+ telefono: {telefono}
+ <p className="text-gray-700 mb-1"></p>
+ correo: {correo}
+ <p className="text-gray-700 mb-1"></p>
+  Empresa: {empresa}
+  <p className="text-gray-700 mb-1"></p>
+  Etiqueta: {etiqueta}
+
+{/* acciones (alineado a la izqueierda en movil) */}  
+ <div className="flex justify-start">
+ <button
+ onClick={() => onEliminar(correo)}
+ className="bg-red-500 hover:bg-red-600 text-white text-sm font-medium
+px-4 py-2 rounded-lg transition-colors"
+ >
+ Eliminar
+ </button>
+ </div>
+ </article>
+ );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import App from './App.jsx'
 import './index.css'
 // Importamos los estilos globales
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
